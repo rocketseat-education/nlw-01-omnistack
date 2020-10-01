4899585d30d5d8f8aae07bccee30454db8e3fb6f
brenu <bvsousa64@gmail.com>
2020-10-01 11:53:44 -0300

    :rocket: Change API endpoint query in order to show UFs ordered by name

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -64,7 +64,7 @@ const CreatePoint = () => {
   }, []);
 
   useEffect(() => {
-    axios.get<IBGEUFResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados').then(response => {
+    axios.get<IBGEUFResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome').then(response => {
       const ufInitials = response.data.map(uf => uf.sigla);
 
       setUfs(ufInitials);
